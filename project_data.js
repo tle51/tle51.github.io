d7345ae45fba0af17b46fb27127685844e09fbbf
Tan Le <tle51@uic.edu>
2017-11-03 16:30:47 +0000

    porfolio submission

---
+++ project_data.js
@@ -0,0 +1,57 @@
+var projects = [
+    //Project 1
+    {
+        "name": 'Simple "About Me" Page',
+        "link": "https://tle51.github.io/IT202/project1/index.html",
+        "picture": "img/project-image/proj1-img.png",
+        "id": "portfolioModal1",
+        "desc": "A simple web app that use basic HTML and CSS to make a profile page. Also include link to personal top 3 TV shows.",
+        "tool": "HTML, CSS",
+        "imgtag": "proj1",
+        "github": "https://github.com/tle51/IT202/tree/master/project1"
+    },
+    //Project 2
+    {
+        "name": 'Using a Bootstrap Template/Theme',
+        "link": "https://tle51.github.io/IT202/project2/index.html",
+        "picture": "img/project-image/proj2-img.png",
+        "id": "portfolioModal2",
+        "desc": "Using bootstrap template and modifying it.",
+        "tool": "Bootstrap, Bootstrap Template",
+        "imgtag": "proj2",
+        "github": "https://github.com/tle51/IT202/tree/master/project2"
+    },
+    //Project 3
+    {
+        "name": 'Data App With User Filter',
+        "link": "https://tle51.github.io/IT202/project3/index.html",
+        "picture": "img/project-image/proj3-img.png",
+        "id": "portfolioModal3",
+        "desc": "Get user input and then filter API request in order to display result in list and Google Map.",
+        "tool": "Google Map API, API, Bootstrap 4, AJAX, jQuery, JavaScript",
+        "imgtag": "proj3",
+        "github": "https://github.com/tle51/IT202/tree/master/project3"
+    },
+    //Project 4
+    {
+        "name": 'Simple Scrolling Game',
+        "link": "https://tle51.github.io/IT202/project4/index.html",
+        "picture": "img/project-image/proj4-img.png",
+        "id": "portfolioModal4",
+        "desc": "Use Canvas to make a simple scrolling game. Objective of the game is to collect green circles but avoid red circle.",
+        "tool": "Canvas, jQuery",
+        "imgtag": "proj4",
+        "github": "https://github.com/tle51/IT202/tree/master/project4"
+    },
+    //Project 5
+    {
+        "name": 'First Progressive Web App',
+        "link": "https://tle51.github.io/IT202/project5/index.html",
+        "picture": "img/project-image/proj5-img.png",
+        "id": "portfolioModal5",
+        "desc": "Building a simple Progressive Web Application that display the weather.",
+        "tool": "PWA",
+        "imgtag": "proj5",
+        "github": "https://github.com/tle51/IT202/tree/master/project5"
+    }
+];
